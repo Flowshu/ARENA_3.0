1a6d04a854f8e9646e8bbb57c86be01becda2b6c
Flowshu <floowshu@gmail.com>
2026-01-06 13:37:31 +0100

    implement MLP

--- chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers.py
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers.py
@@ -267,3 +267,25 @@ class Attention(nn.Module):
 tests.test_causal_mask(Attention.apply_causal_mask)
 rand_float_test(Attention, [2, 4, 768])
 load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["normalized", 0, "ln1"])
+
+# %%
+class MLP(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(t.empty((cfg.d_model, cfg.d_mlp)))
+        self.W_out = nn.Parameter(t.empty((cfg.d_mlp, cfg.d_model)))
+        self.b_in = nn.Parameter(t.zeros((cfg.d_mlp)))
+        self.b_out = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_in, std=self.cfg.init_range)
+        nn.init.normal_(self.W_out, std=self.cfg.init_range)
+
+    def forward(
+        self, normalized_resid_mid: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        h = einops.einsum(normalized_resid_mid, self.W_in, "batch posn d_model, d_model d_mlp -> batch posn d_mlp") + self.b_in
+        h = gelu_new(h)
+        return einops.einsum(h, self.W_out, "batch posn d_mlp, d_mlp d_model -> batch posn d_model") + self.b_out
+
+rand_float_test(MLP, [2, 4, 768])
+load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["normalized", 0, "ln2"])
